91df887427a66617f41cb6f6f588dcb46b7f8cd4
Filip Budiša <filip.budisa@gmail.com>
2019-12-18 18:41:11 +0100

    Add env to options in index.d.ts Fixes #117

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,1 @@
-export function exec(cmd: string, options: { name?: string, icns?: string }, callback: (error: string, stdout: string, stderr: string) => void): void;
+export function exec(cmd: string, options: { name?: string, icns?: string, env?: { [key: string]: string } }, callback: (error: string, stdout: string, stderr: string) => void): void;
